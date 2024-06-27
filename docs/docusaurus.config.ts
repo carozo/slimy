1be0bd4d583893672f6c29ec4e22d0830ca65a07
Belen Carozo <belcarozo@gmail.com>
2024-06-27 13:57:37 -0300

    gh pages test

--- docs/docusaurus.config.ts
+++ docs/docusaurus.config.ts
@@ -9,7 +9,7 @@ const config: Config = {
 
   // Set the production url of your site here
   url: 'https://carozo.github.io',
-  // Set the /<baseUrl>/ pathname under which your site is served
+  // Set the /<baseUrl>/ pathname under which your  site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/slimy',
 
